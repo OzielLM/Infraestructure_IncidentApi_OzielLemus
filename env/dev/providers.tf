f61745a78e273fcc171fa1a7d15995a35c5fb053
Oziel Lemus <114196082+OzielLM@users.noreply.github.com>
2024-10-09 17:43:50 -0600

    Update program 3

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+
+  backend "azurerm"{
+    resource_group_name = "Blob-RG-OzielLemus"
+    storage_account_name = "blobsaoziellemus"
+    container_name = "bloboziellemus"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
